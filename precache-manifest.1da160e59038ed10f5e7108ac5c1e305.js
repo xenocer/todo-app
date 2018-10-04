dde279427012eb3136c87aa39ca1e84e8e6a800f
xenocer <xenocer@users.noreply.github.com>
2018-10-05 03:00:28 +0700

    Updates

---
+++ precache-manifest.1da160e59038ed10f5e7108ac5c1e305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "1a993488455e8386a782",
+    "url": "/todo-app/static/js/main.1a993488.chunk.js"
+  },
+  {
+    "revision": "8f83cbe3f7bfb210abfc",
+    "url": "/todo-app/static/js/1.8f83cbe3.chunk.js"
+  },
+  {
+    "revision": "1a993488455e8386a782",
+    "url": "/todo-app/static/css/main.85395159.chunk.css"
+  },
+  {
+    "revision": "8f83cbe3f7bfb210abfc",
+    "url": "/todo-app/static/css/1.2be24372.chunk.css"
+  },
+  {
+    "revision": "09241a46127caabc552e1cf8b5573387",
+    "url": "/todo-app/index.html"
+  }
+];
